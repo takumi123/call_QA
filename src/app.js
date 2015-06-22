15e2aea396898fed8941349e25b89bd7e4132de2
takumi123 <takumi.endoh111@gmail.com>
2015-06-23 02:03:35 +0900

    クッキー、セッション追加

--- src/app.js
+++ src/app.js
@@ -4,6 +4,9 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var session = require("express-session");
+
+
 //↑node_modulesにあるファイル
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -24,6 +27,13 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+  secret:"hogehoge",
+  resave:false,//sessionストアに保存を強制
+  saveUninitialized : true,
+
+}));
+
 
 app.use('/', routes);
 app.use('/users', users);//users直下はこのルーティングを行う。
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,14 +1,61 @@
 var express = require('express');
 var router = express.Router();
 
+var member = {
+  taro:"yamada",
+  hanako:"flower"
+};
+
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index',{title:"Hello World",body:"こんにちは"});
+  if(req.session.login==true)res.redirect("/login");
+  res.render('index');
+
+});
+
+router.post('/login', function(req, res, next) {
+  var user = req.body.user;
+  var password = req.body.password;
+  var pass_member = member[user];
+
+  if(pass_member==password){
+    req.session.login = true;
+    req.session.name = user;
+    req.session.msg = '"'+user+'"でログインしました';
+  }else{
+    req.session.login = false;
+    req.session.msg = "ログインに失敗しました";
+  };
+
+  res.render('login',{msg:req.session.msg});
+
+});
+
+router.get('/login', function(req, res, next) {
+  if(req.session.login==true){
+    res.set('Content-Type', 'text/html');
+    res.status(200);
+    res.render("login",{msg:req.session.msg});
+  }else{
+    res.redirect("/");
+  }
+
+
 });
 
 
-router.post("/create",function(req,res,next){
-  res.render("form",{value:req.body.name1});
+router.get("/1",function(req,res,next){
+
+
+  var cookie = req.cookies.cookie_name;
+  if(cookie == req.session.value){
+    res.send("認証成功");
+  }else{
+    res.send("認証失敗");
+  };
+
 });
 
 module.exports = router;
